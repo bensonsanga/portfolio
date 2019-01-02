9300fc3cd2d02092fc1c37751c94872404700f88
Benson Sanga <bensonsanga@hotmail.com>
2019-01-02 15:22:04 +0300

    Updates

---
+++ precache-manifest.47d23edbe5b1691350f2bb40cddedfe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8672e625fc34dc8e9bf0",
+    "url": "/portfolio/static/css/main.dbfb6a93.chunk.css"
+  },
+  {
+    "revision": "8672e625fc34dc8e9bf0",
+    "url": "/portfolio/static/js/main.8672e625.chunk.js"
+  },
+  {
+    "revision": "4065a48a2f32cd9395fd",
+    "url": "/portfolio/static/css/1.5ab9991f.chunk.css"
+  },
+  {
+    "revision": "4065a48a2f32cd9395fd",
+    "url": "/portfolio/static/js/1.4065a48a.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3100088801d9ea50af7d2a8275cdc7f8",
+    "url": "/portfolio/static/media/Avatar.31000888.jpg"
+  },
+  {
+    "revision": "22f3cb1f1de87a4fd1481884d21d32af",
+    "url": "/portfolio/index.html"
+  }
+];
