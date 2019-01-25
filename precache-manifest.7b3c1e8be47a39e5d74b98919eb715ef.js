1efa4171b58dbdc61fec8b8bfe41162f2dbfbe91
Benson Sanga <bensonsanga@hotmail.com>
2019-01-25 17:10:30 +0300

    Updates

---
+++ precache-manifest.7b3c1e8be47a39e5d74b98919eb715ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a893ea558a6c7f208d3",
+    "url": "/static/css/main.0f2dfb6c.chunk.css"
+  },
+  {
+    "revision": "8a893ea558a6c7f208d3",
+    "url": "/static/js/main.8a893ea5.chunk.js"
+  },
+  {
+    "revision": "a7341ed82419620ca771",
+    "url": "/static/css/1.8639284b.chunk.css"
+  },
+  {
+    "revision": "a7341ed82419620ca771",
+    "url": "/static/js/1.a7341ed8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e7be8002c1d51b314df0f8b6aa36eb7",
+    "url": "/static/media/logo.0e7be800.svg"
+  },
+  {
+    "revision": "3100088801d9ea50af7d2a8275cdc7f8",
+    "url": "/static/media/Avatar.31000888.jpg"
+  },
+  {
+    "revision": "b5602470770f37d317e29f704f211821",
+    "url": "/index.html"
+  }
+];
